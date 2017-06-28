c395b4f138d28998f1e87040c1ee468733e6ece8
joshua wood <joshua wood>
2017-06-28 17:01:42 -0400

    switched port

--- server.js
+++ server.js
@@ -110,6 +110,6 @@ app.get("/", function(req, res){
 	res.sendFile(path.join(__dirname + "./public/index.html"));
 });
 
-app.listen(3000, function(){
+app.listen(8080, function(){
 	console.log("Running On Port 3000");
 });
